baebb7f879e1479a086a4384bc11be1f22c05c96
Beheta13 <angenathanf@gmail.com>
2025-11-27 14:28:59 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, signal, computed } from '@angular/core';
+import { POKEMON_LIST } from './pokemon-list.fake';
+import { Pokemon } from './pokemon.model';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  pokemonList = signal(POKEMON_LIST);
+  size (pokemon: Pokemon) {
+    if (pokemon.life <= 15) {
+      return 'Petit';
+    }
+
+    if (pokemon.life >=25) {
+      return 'Grand';
+    }
+
+    return 'Moyen';
+  };
+  
+  incrementLife(pokemon: Pokemon) {
+    pokemon.life = pokemon.life + 1;
+  }
+  decrementLife(pokemon: Pokemon) {
+    pokemon.life = pokemon.life - 1;
+  }
+
+}
